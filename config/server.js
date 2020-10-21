608b9b2681d8a42e34a9cf9fdc561800992ad6c1
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2020-10-21 13:40:44 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '95053471d9575b81ded8ef412a2c2cbe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4cd1565c-02c0-4402-80b3-f6ac6bddf527'
+};
