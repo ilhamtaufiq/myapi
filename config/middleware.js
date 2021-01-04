c2db2e2f4012b4e25aea50a8f9aef1dd28230c99
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-05 00:36:06 +0700

    cors

---
+++ config/middleware.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //...
+    settings: {
+      cors: {
+        origin: ['http://localhost:8080', 'https://abs-dpkpp.herokuapp.com', 'https://www.apiabs.herokuapp.com'],
+      },
+    },
+  };
+  
