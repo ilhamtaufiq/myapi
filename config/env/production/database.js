a0be63eabb20f4ed402adcd6e4d726f993291781
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-04 17:26:53 +0700

    Update database config

---
+++ config/env/production/database.js
@@ -0,0 +1,21 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+      },
+      options: {},
+    },
+  },
+});
+
