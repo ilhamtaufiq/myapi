bbf40638784217ea8f7abd830149a371c62820c1
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-05 08:27:47 +0700

    cors failed

--- config/middleware.js
+++ config/middleware.js
@@ -4,7 +4,7 @@ module.exports = {
       cors: {
         enabled: true,
         headers: '*',
-        origin: ['http://localhost:8080', 'https://abs-dpkpp.herokuapp.com', 'https://www.apiabs.herokuapp.com'],
+        origins: '*',
       },
     },
   };
