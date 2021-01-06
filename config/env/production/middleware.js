b463894f40498eb2a8e6182d96849a4800cb2009
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-06 18:17:07 +0700

    cors

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -87,7 +87,7 @@ module.exports = {
         cors: {
           enabled: true,
           origin: ['*'], // ['*'] to allow all origins
-          headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'x-csrf-token'], // ['*'] to allow all headers
+          headers: ['*'], // ['*'] to allow all headers
     
         },
         ip: {
