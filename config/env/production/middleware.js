5cf28475e04cd7ceee71714d2eb56996b9344e9a
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-06 18:29:02 +0700

    tes

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,6 +1,13 @@
 module.exports = {
-    settings: {
-      cors: { enabled: true, headers: '*', origin: '*' }
-    },
-  };
-  
+  timeout: 100,
+  load: {
+    before: ["cors", "logger", "responseTime", "responses", "gzip"],
+    order: [
+      "Define the middlewares' load order by putting their name in this array is the right order",
+    ],
+    after: ["parser", "router"],
+  },
+  settings: {
+    cors: { enabled: true, headers: '*', origin: '*' }
+  },
+};
