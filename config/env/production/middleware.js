c26e8282d673b4ed6cc4521f4fc4f2477ffe8699
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-07 11:39:26 +0700

    text

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,13 +1,12 @@
 module.exports = {
-  timeout: 100,
-  load: {
-    before: ["cors", "logger", "responseTime", "responses", "gzip"],
-    order: [
-      "Define the middlewares' load order by putting their name in this array is the right order",
-    ],
-    after: ["parser", "router"],
-  },
+  //...
   settings: {
-    cors: { enabled: true, headers: '*', origin: '*' }
-  },
+    cors: {
+      enabled: true, //<--
+      origin: ['http://localhost', 'https://mysite.com', 'https://www.mysite.com'], // ['*'] to allow all origins
+      headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'x-csrf-token'], // ['*'] to allow all headers
+    },
+  }
+  //...
 };
+
