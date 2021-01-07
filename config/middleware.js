1b49d30e1fbc6bfaccae1b6e2d5fe4b91be11621
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2021-01-07 14:06:32 +0700

    cors fixed

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   settings: {
     cors: {
       enabled: true,
-      origin: ['http://localhost:8080'],
+      origin: ['http://localhost:8080', 'https://abs-dpkpp.herokuapp.com'],
       headers: [
         'Content-Type',
         'Authorization',
